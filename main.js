d2f2109c39fc40ed1db6b760a8d99da94d0df121
Evander Inácio <evander.20112@hotmail.com>
2022-03-04 13:24:14 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,20 @@
 
+
 const navMenu = document.getElementById('menu-mobile');
 
 function toggleMenu(){
   const nav = document.getElementById('nav');
   nav.classList.toggle('active');
+}
+
+
+//remover ao clicar em algum "A" do menu mobile
+const links = document.querySelectorAll('nav ul li a');
+
+for(const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('active');
+  })
 }
 
 
